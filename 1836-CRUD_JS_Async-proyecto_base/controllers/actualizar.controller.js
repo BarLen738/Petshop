c74c258940f7ab97a250642a2a8780ec5fd9aff7
BarLen738 <barbara.lencinas@gmail.com>
2022-11-14 11:52:24 -0300

    Aula 5 terminada

--- 1836-CRUD_JS_Async-proyecto_base/controllers/actualizar.controller.js
+++ 1836-CRUD_JS_Async-proyecto_base/controllers/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector('[data-form]');
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => { //esperar la respuesta
     const url = new URL(window.location);
     const id = url.searchParams.get('id');
 
@@ -13,14 +13,18 @@ const obtenerInformacion = () => {
     const nombre = document.querySelector('[data-nombre]');
     const email = document.querySelector('[data-email]');
 
-
-
-    clientServices.detalleCliente(id)
-    .then( (perfil) => {
-        nombre.value = perfil.nombre;
-        email.value = perfil.email;
-
-    });
+    try { //ejecutar respuesta
+        const perfil = await clientServices.detalleCliente(id); //la respuesta que obtiene la asigna a la constante perfil
+        if (perfil.nombre && perfil.email) {
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        } else {
+            throw new error();
+        }
+    }
+    catch (error) {
+        window.location.href = '../screens/error.html'
+    }
 };
 
 obtenerInformacion();
@@ -29,11 +33,11 @@ formulario.addEventListener('submit', (evento) => {
     evento.preventDefault(); //prevenir el funcionamiento normal del formulario, para que no realice la petición al servidor
     const url = new URL(window.location);
     const id = url.searchParams.get('id');
-    
+
     const nombre = document.querySelector('[data-nombre]').value;
     const email = document.querySelector('[data-email]').value;
     clientServices.actualizarCliente(nombre, email, id)
-    .then ( () => {
-        window.location.href = '../screens/edicion_concluida.html';
-    });
+        .then(() => {
+            window.location.href = '../screens/edicion_concluida.html';
+        });
 });
